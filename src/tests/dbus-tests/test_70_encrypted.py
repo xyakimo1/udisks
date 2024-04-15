cf3ea3e32c3e1d1d959c4e354f1d3937393a6744
Stepan "Sciapan" Yakimovich <xyakimo1@fi.muni.cz>
2024-04-15 19:54:13 +0000

    Added Restore method, but the test doesn't work.

--- src/tests/dbus-tests/test_70_encrypted.py
+++ src/tests/dbus-tests/test_70_encrypted.py
@@ -609,7 +609,7 @@ class UdisksEncryptedTestLUKS2(UdisksEncryptedTest):
 
         self.assertEqual(2, _get_luks_version(disk))
 
-    def test_header_backup(self):
+    def test_header_backup_restore(self):
         disk = self.vdevs[0]
         device = self.get_device(disk)
         self._create_luks(device, self.PASSPHRASE)
@@ -635,6 +635,20 @@ class UdisksEncryptedTestLUKS2(UdisksEncryptedTest):
 
         self.assertTrue(os.path.exists(BACKUP_FILE))
 
+        # check that after reaping device and restoring header, cryptsetup will recognize header
+        device.Lock(self.no_options, dbus_interface=self.iface_prefix + '.Encrypted')
+        ret, out = udiskstestcase.UdisksTestCase.run_command("cryptsetup luksDump %s" % disk)
+        self.assertEqual(ret, 0)
+        DISK_SIZE = 100 # magic number from `targetcli_config.json`
+        ret, out = udiskstestcase.UdisksTestCase.run_command("dd if=/dev/zero of=%s bs=1M count=%d" % (disk, DISK_SIZE))
+        self.assertEqual(ret, 0)
+        ret, out = udiskstestcase.UdisksTestCase.run_command("cryptsetup luksDump %s" % disk)
+        self.assertEqual(1, ret)
+        self.assertTrue(("Device %s is not a valid LUKS device." % disk) in out)
+
+        device = self.get_device(disk)
+        device.HeaderRestore(BACKUP_FILE, self.no_options,
+                             dbus_interface=self.iface_prefix + '.Encrypted')
 
     def _get_default_luks_version(self):
         manager = self.get_object('/Manager')
